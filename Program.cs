0da52339410b148062f103576045948b3f4065c2
seangdy085@gmail.com <makaramarady.seang@techbodia.local>
2025-01-21 09:42:26 +0700

    add: dependencies injection and config the app json

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+using TaskManagementSystem.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Register DB Context
+builder.Services.AddScoped<IDbContext>(sp =>
+    new DbContext(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 

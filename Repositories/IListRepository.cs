81ca4130cab58c8ecd1ea783cdcebb21c397ac4b
seangdy085@gmail.com <makaramarady.seang@techbodia.local>
2025-01-21 10:39:12 +0700

    add: implement the List repo

--- Repositories/IListRepository.cs
+++ Repositories/IListRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace TaskManagementSystem.Repositories;
+﻿using TaskManagementSystem.Models;
+
+namespace TaskManagementSystem.Repositories;
 
 public interface IListRepository
 {
-    
+    Task<Guid> CreateListAsync(List list);
+    Task<IEnumerable<List>> GetBoardListsAsync(Guid boardId);
+    Task UpdateListPositionAsync(Guid listId, int newPosition);
+    Task UpdateListTitleAsync(Guid listId, string newTitle);
+    Task DeleteListAsync(Guid listId);
 }
